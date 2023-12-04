59fe5871f2c4761131e805ee0b5cccb802c4b474
habbak <taous.habbak@etu.u-paris.fr>
2023-12-05 00:26:34 +0100

    Segment

--- lib/src/main/java/poo/Main.java
+++ lib/src/main/java/poo/Main.java
@@ -1,7 +1,7 @@
 package poo;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -9,14 +9,27 @@ public class Main extends Application {
     private static final int CELL_SIZE = 10;
     private static final int GRID_WIDTH = 20;
     private static final int GRID_HEIGHT = 20;
-    
+
     @Override
     public void start(Stage primaryStage) {
-        
+
+        // Utilisation d'un BorderPane comme conteneur principal
+        BorderPane root = new BorderPane();
+
+        // Création d'un Plateau et ajout au centre du BorderPane
         Plateau plateau = new Plateau();
-        Scene scene = new Scene(new BorderPane(plateau), GRID_WIDTH * CELL_SIZE, GRID_HEIGHT * CELL_SIZE);
-         primaryStage.setScene(scene);
-        //Label label = new Label("Hello, JavaFX!");
+        
+        root.setCenter(plateau);
+
+        // Création d'une scène avec le BorderPane
+       Scene scene = new Scene(new BorderPane(plateau), GRID_WIDTH * CELL_SIZE, GRID_HEIGHT * CELL_SIZE);
+        // Création d'un joueur avec un Segment
+        Segment playerSegment = new Segment(new Position<>(0, 0));
+        
+        // Ajout du Segment à la liste des enfants de la scène
+        root.getChildren().add(playerSegment.getView());
+
+        // Configuration de la scène et affichage de la fenêtre principale
         primaryStage.setScene(scene);
         primaryStage.setTitle("Lyes");
         primaryStage.show();
