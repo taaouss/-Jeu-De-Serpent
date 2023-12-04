c4b16064d6f9b041bba755b821f524f13fcb1545
habbak <taous.habbak@etu.u-paris.fr>
2023-12-04 23:35:17 +0100

    Plateau

--- lib/src/main/java/poo/Main.java
+++ lib/src/main/java/poo/Main.java
@@ -2,15 +2,23 @@ package poo;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static final int CELL_SIZE = 10;
+    private static final int GRID_WIDTH = 20;
+    private static final int GRID_HEIGHT = 20;
+    
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("Hello, JavaFX!");
-        Scene scene = new Scene(label, 300.0, 200.0);
+        
+        Plateau plateau = new Plateau();
+        Scene scene = new Scene(new BorderPane(plateau), GRID_WIDTH * CELL_SIZE, GRID_HEIGHT * CELL_SIZE);
+         primaryStage.setScene(scene);
+        //Label label = new Label("Hello, JavaFX!");
         primaryStage.setScene(scene);
-        primaryStage.setTitle("JavaFX Application");
+        primaryStage.setTitle("Lyes");
         primaryStage.show();
     }
 
